2beae206bcb688fa756fc886ed4e4ea42405049c
M4tyu633 <matthewtlabrador@gmail.com>
2026-09-21 18:05:39 +0800

    Compare the failing foot readback against bone skinning and scale compensation

--- Assets/TumbangPreso/Tests/PlayMode/FootSupportProbe.cs
+++ Assets/TumbangPreso/Tests/PlayMode/FootSupportProbe.cs
@@ -150,6 +150,22 @@ namespace TumbangPreso.PlayTests
                         records.Add(string.Format(CultureInfo.InvariantCulture,"{0},{1:F4},{2:F4},{3:F4},{4}",stage,who.transform.position.y,sole,support,who.IsGrounded));
                         if(planted && Mathf.Abs(support-sole)>.025f)
                         {
+                            foreach(var skin in who.GetComponentsInChildren<SkinnedMeshRenderer>().Where(s=>s.bones.Any(b=>b!=null&&b.name=="leg-left")))
+                            {
+                                var vertices=skin.sharedMesh.vertices;var weights=skin.sharedMesh.boneWeights;var binds=skin.sharedMesh.bindposes;
+                                float manual=float.PositiveInfinity;
+                                for(int v=0;v<vertices.Length;v++)
+                                {
+                                    var weight=weights[v];var world=Vector3.zero;
+                                    world+=Point(weight.boneIndex0,weight.weight0);world+=Point(weight.boneIndex1,weight.weight1);
+                                    world+=Point(weight.boneIndex2,weight.weight2);world+=Point(weight.boneIndex3,weight.weight3);
+                                    manual=Mathf.Min(manual,world.y);
+                                    Vector3 Point(int index,float amount)=>amount<=0?Vector3.zero:skin.bones[index].TransformPoint(binds[index].MultiplyPoint3x4(vertices[v]))*amount;
+                                }
+                                skin.BakeMesh(mesh,true);float scaled=float.PositiveInfinity;
+                                foreach(var v in mesh.vertices)scaled=Mathf.Min(scaled,skin.transform.TransformPoint(v).y);
+                                Debug.Log("[FootSupport] skin="+skin.name+" scale="+skin.transform.lossyScale+" manualSkinMin="+manual+" compensatedBakeMin="+scaled);
+                            }
                             foreach(var part in who.GetComponentsInChildren<Transform>())
                                 if(part.name=="Visual"||part.name=="root"||part.name=="leg-left"||part.name=="leg-right")
                                     Debug.Log("[FootSupport] "+part.name+" parent="+part.parent?.name+" world="+part.position.ToString("F4")+" local="+part.localPosition.ToString("F4"));
